70a26a140c593559924560ab0cb89b39adf41a66
Nrittik Sarmah <nrittik43@gmail.com>
2022-04-20 21:13:38 +0530

    2nd year 2nd round

---
+++ Second years Round 2/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("mousemove", (e) => {
+  const sqrs = document.querySelectorAll(".item");
+
+  const mouseX = e.pageX;
+  const mouseY = e.pageY;
+
+  sqrs.forEach((sqr) => {
+    const sqrX = sqr.offsetLeft;
+    const sqrY = sqr.offsetTop;
+
+    const diffX = mouseX - sqrX;
+    const diffY = mouseY - sqrY;
+    const angle = (Math.atan2(diffY, diffX) * 180) / Math.PI;
+
+    sqr.style.transform = `rotateY(${angle}deg)`;
+  });
+});
